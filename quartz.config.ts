45ae244339246b90ca2e5a22e628f548bc5eeada
Bhargav Limbasia <blimbasi@usc.edu>
2026-03-29 19:35:35 -0700

    Quartz sync: Mar 29, 2026, 7:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "bhargavlimbasia.github.io/portfolio-quartz",
+    baseUrl: "bhargavlimbasia.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
